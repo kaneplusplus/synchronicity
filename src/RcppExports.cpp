9c335307dc055d5f99c519de07df9a753f71ae92
Michael J. Kane <kaneplusplus@gmail.com>
2024-01-02 11:08:03 -0500

    Overdue updates.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // CreateBoostMutexInfo
 SEXP CreateBoostMutexInfo(SEXP resourceName, SEXP timeout);
 RcppExport SEXP _synchronicity_CreateBoostMutexInfo(SEXP resourceNameSEXP, SEXP timeoutSEXP) {
